b5c9b2a4ab3c8b14f12e05ae2f0cddd8f63caa16
Tim <book9902@yandex.ru>
2023-04-03 01:13:23 +0300

    fixCode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,90 +3,34 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     static BlockingQueue<String> blockingQueueA = new ArrayBlockingQueue<>(100);
     static BlockingQueue<String> blockingQueueB = new ArrayBlockingQueue<>(100);
     static BlockingQueue<String> blockingQueueC = new ArrayBlockingQueue<>(100);
     static List<Thread> threadList = new ArrayList<>();
-    static int maxCountA;
-    static int maxCountB;
-    static int maxCountC;
-    static String A;
-    static String B;
-    static String C;
+    static int maxCount;
+    static String name;
 
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 100_0; i++) {
-            new Thread(() -> {
-                String str = generateText("abc", 1000);
-                try {
-                    blockingQueueA.put(str);
-                    blockingQueueB.put(str);
-                    blockingQueueC.put(str);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+    protected static void thread(BlockingQueue<String> blockingQueue, char charName) {
+        Runnable runnable = () -> {
+            try {
+                String str = blockingQueue.take();
+                int count = (int) str.chars().filter(ch -> ch == charName).count();
+                if (count > maxCount) {
+                    name = str;
+                    maxCount = count;
                 }
-            }).start();
-            Runnable runnableA = () -> {
-                try {
-                    String str = blockingQueueA.take();
-                    long count = str.chars().filter(ch -> ch == 'a').count();
-                    if (count > maxCountA) {
-                        A = str;
-                        maxCountA = (int) count;
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            };
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
 
-            Thread threadA = new Thread(runnableA);
-            threadList.add(threadA);
-            threadA.start();
-
-            Runnable runnableB = () -> {
-                try {
-                    String str = blockingQueueB.take();
-                    long count = str.chars().filter(ch -> ch == 'b').count();
-                    if (count > maxCountB) {
-                        B = str;
-                        maxCountB = (int) count;
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            };
-
-            Thread threadB = new Thread(runnableB);
-            threadList.add(threadB);
-            threadB.start();
-
-            Runnable runnableC = () -> {
-                try {
-                    String str = blockingQueueC.take();
-                    long count = str.chars().filter(ch -> ch == 'c').count();
-                    if (count > maxCountC) {
-                        C = str;
-                        maxCountC = (int) count;
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            };
-            Thread threadC = new Thread(runnableC);
-            threadList.add(threadC);
-            threadC.start();
-        }
-        for (Thread thread : threadList) {
-            thread.join();
-        }
-        System.out.println("Количество а = " + maxCountA);
-        System.out.println(A);
-        System.out.println("Количество b = " + maxCountB);
-        System.out.println(B);
-        System.out.println("Количество c = " + maxCountC);
-        System.out.println(C);
+        Thread thread = new Thread(runnable);
+        threadList.add(thread);
+        thread.start();
     }
 
     public static String generateText(String letters, int length) {
@@ -96,5 +40,30 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            String str = generateText("abc", 1000);
+            for (int i = 0; i < 1000; i++) {
+                try {
+                    blockingQueueA.put(str);
+                    blockingQueueB.put(str);
+                    blockingQueueC.put(str);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        thread(blockingQueueA, 'a');
+        thread(blockingQueueB, 'b');
+        thread(blockingQueueC, 'c');
+
+        for (Thread thread : threadList) {
+            thread.join();
+        }
+        System.out.println(maxCount);
+        System.out.println(name);
     }
 }
